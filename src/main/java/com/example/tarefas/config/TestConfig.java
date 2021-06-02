edacc422c2152da45aa4dc3c31dd7d335a0f913b
Fernando Fritzen <fernandofritzen9@gmail.com>
2021-06-01 21:36:01 -0300

    codigo final

--- src/main/java/com/example/tarefas/config/TestConfig.java
+++ src/main/java/com/example/tarefas/config/TestConfig.java
@@ -26,7 +26,7 @@ public class TestConfig implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Cliente c1 = new Cliente(null, "Clovis Basilio");
-        Cliente c2 = new Cliente(null, "Mia Khalifa");
+        Cliente c2 = new Cliente(null, "Fabio Junior");
 
         Tarefa t1 = new Tarefa(null, "fazer relatório", "Lançar relatório no Excel", c1);
         Tarefa t2 = new Tarefa(null, "marcar reunião", "Marcar Reunião com o Bill Gates", c2);
